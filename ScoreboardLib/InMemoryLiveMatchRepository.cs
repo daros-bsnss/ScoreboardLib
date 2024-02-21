8ba23eecd7bc43a1945a5b0d3be33afcf2525c65
Daria Osipova <osipov.daria@gmail.com>
2024-02-21 17:49:51 +0100

    add in-memory live match repository implementation

--- ScoreboardLib/InMemoryLiveMatchRepository.cs
+++ ScoreboardLib/InMemoryLiveMatchRepository.cs
@@ -1,5 +1,6 @@
 ﻿using ScoreboardLib.Interfaces;
 using ScoreboardLib.Models;
+using System;
 using System.Collections.Generic;
 
 namespace ScoreboardLib
@@ -11,6 +12,39 @@ namespace ScoreboardLib
         public IEnumerable<LiveMatch> GetAll()
         {
             return matches;
+        }
+
+        public Guid Insert(LiveMatch match)
+        {
+            match.Id = Guid.NewGuid();
+            matches.Add(match);
+            return match.Id;
+        }
+
+        public bool Update(LiveMatch match)
+        {
+            var storedMatch = matches.Find(m => m.Id == match.Id);
+
+            if (storedMatch is null)
+            {
+                return false;
+            }
+
+            storedMatch.Score = match.Score;
+            return true;
+        }
+
+        public bool Delete(Guid matchId)
+        {
+            var storedMatch = matches.Find(m => m.Id == matchId);
+
+            if (storedMatch is null)
+            {
+                return false;
+            }
+
+            matches.Remove(storedMatch);
+            return true;
         }
     }
 }
--- ScoreboardLib/Interfaces/ILiveMatchRepository.cs
+++ ScoreboardLib/Interfaces/ILiveMatchRepository.cs
@@ -1,4 +1,5 @@
 ﻿using ScoreboardLib.Models;
+using System;
 using System.Collections.Generic;
 
 namespace ScoreboardLib.Interfaces
@@ -6,5 +7,8 @@ namespace ScoreboardLib.Interfaces
     public interface ILiveMatchRepository
     {
         public IEnumerable<LiveMatch> GetAll();
+        public Guid Insert(LiveMatch match);
+        public bool Update(LiveMatch match);
+        public bool Delete(Guid matchId);
     }
 }

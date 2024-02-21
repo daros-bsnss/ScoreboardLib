5a5a5016c514a46aa72644f81399a1aefe1c6a7e
Daria Osipova <osipov.daria@gmail.com>
2024-02-21 17:19:24 +0100

    add insert update delete tests

--- ScoreboardLib.Tests/InMemoryLiveMatchRepositoryTests.cs
+++ ScoreboardLib.Tests/InMemoryLiveMatchRepositoryTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+﻿using ScoreboardLib.Models;
 
 namespace ScoreboardLib.Tests
 {
@@ -17,6 +17,73 @@ namespace ScoreboardLib.Tests
             // Assert
             Assert.IsNotNull(matches);
             Assert.AreEqual(0, matches.Count());
+        }
+
+        [TestMethod]
+        public void Insert_ShouldAddMatch_AndReturnId()
+        {
+            // Arrange
+            var match = new LiveMatch();
+
+            // Act
+            Guid id = repository.Insert(match);
+
+            // Assert
+            Assert.AreNotEqual(Guid.Empty, id);
+        }
+
+        [TestMethod]
+        public void Update_ShouldUpdateAndReturnTrue()
+        {
+            // Arrange
+            var match = new LiveMatch();
+            Guid id = repository.Insert(match);
+
+            // Act
+            bool isUpdated = repository.Update(id, 1, 0);
+
+            // Assert
+            Assert.IsTrue(isUpdated);
+        }
+
+        [TestMethod]
+        public void Update_ShouldReturnFalse_WhenMatchNotFound()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+
+            // Act
+            bool isUpdated = repository.Update(id, 1, 0);
+
+            // Assert
+            Assert.IsFalse(isUpdated);
+        }
+
+        [TestMethod]
+        public void Delete_ShouldDeleteAndReturnTrue()
+        {
+            // Arrange
+            var match = new LiveMatch();
+            Guid id = repository.Insert(match);
+
+            // Act
+            bool isDeleted = repository.Delete(id);
+
+            // Assert
+            Assert.IsTrue(isDeleted);
+        }
+
+        [TestMethod]
+        public void Delete_ShouldReturnFalse_WhenMatchNotFound()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+
+            // Act
+            bool isDeleted = repository.Delete(id);
+
+            // Assert
+            Assert.IsFalse(isDeleted);
         }
     }
 }

d35e45c6d871279f8220c7460ac5ec1931ac96ed
Daria Osipova <osipov.daria@gmail.com>
2024-02-21 22:04:34 +0100

    add update validator

---
+++ ScoreboardLib/Validation/UpdateLiveMatchValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using ScoreboardLib.Validation;
+using System;
+
+namespace ScoreboardLib.Models
+{
+    public class UpdateLiveMatchValidator : CreateLiveMatchValidator
+    {
+        public UpdateLiveMatchValidator() : base()
+        {
+            RuleFor(m => m.Id).Must(i => i != Guid.Empty);
+        }
+    }
+}

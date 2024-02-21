228b1baf5f33072232930ad87b4a0f21b36fe445
Daria Osipova <osipov.daria@gmail.com>
2024-02-21 21:35:25 +0100

    add validator for live match for creation

---
+++ ScoreboardLib.Tests/Models/CreateLiveMatchValidatorTests.cs
@@ -0,0 +1,82 @@
+﻿using ScoreboardLib.Models;
+
+namespace ScoreboardLib.Tests.Models
+{
+    [TestClass]
+    public class CreateLiveMatchValidatorTests
+    {
+        private readonly CreateLiveMatchValidator validator = new();
+
+        [TestMethod]
+        public void Validate_ShouldPass()
+        {
+            // Arrange
+            var match = new LiveMatch
+            {
+                HomeTeam = "Germany",
+                AwayTeam = "Chroatia"
+            };
+
+            // Act
+            var validationResult = validator.Validate(match);
+
+            // Assert
+            Assert.IsNotNull(validationResult);
+            Assert.IsTrue(validationResult.IsValid);
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenEmptyTeam()
+        {
+            // Arrange
+            var match = new LiveMatch
+            {
+                HomeTeam = "",
+                AwayTeam = "Chroatia"
+            };
+
+            // Act
+            var validationResult = validator.Validate(match);
+
+            // Assert
+            Assert.IsNotNull(validationResult);
+            Assert.IsFalse(validationResult.IsValid);
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenNullTeam()
+        {
+            // Arrange
+            var match = new LiveMatch
+            {
+                HomeTeam = "Germany",
+                AwayTeam = null
+            };
+
+            // Act
+            var validationResult = validator.Validate(match);
+
+            // Assert
+            Assert.IsNotNull(validationResult);
+            Assert.IsFalse(validationResult.IsValid);
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenTeamsEqual()
+        {
+            // Arrange
+            var match = new LiveMatch
+            {
+                HomeTeam = "Germany   ",
+                AwayTeam = "GeRmanY"
+            };
+
+            // Act
+            var validationResult = validator.Validate(match);
+
+            // Assert
+            Assert.IsNotNull(validationResult);
+            Assert.IsFalse(validationResult.IsValid);
+        }
+    }
+}
